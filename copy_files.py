13a7964d4312e72e0c487bd07298c34e4a9636b1
Hirusha Adikari <hirushaadi@gmail.com>
2024-12-07 22:47:36 +0530

    a

---
+++ copy_files.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+# Destination directory
+dst = r"/home/ubuntu/apps/class-notes/pb/pb_public"
+
+# Show a message to the user
+print("Starting the process...")
+
+# Step 1: Deleting everything inside the destination folder
+print(f"1. Deleting everything inside the '{dst}' folder")
+for filename in os.listdir(dst):
+    file_path = os.path.join(dst, filename)
+    print(file_path)
+    # if os.path.isdir(file_path):
+    #     shutil.rmtree(file_path)
+    # else:
+    #     os.remove(file_path)
+
+# Step 2: Copying the './images' folder to the destination
+print("2. Copying the './images' folder to '{}'".format(dst))
+shutil.copytree('./images', os.path.join(dst, 'images'))
+
+# Step 3: Copying the './subjects' folder to the destination
+print("3. Copying the './subjects' folder to '{}'".format(dst))
+shutil.copytree('./subjects', os.path.join(dst, 'subjects'))
+
+# Success
+print("Process completed successfully!")
